87c1e0d38945b0a5f85f5c5b9d3e3392eddac845
Yury Tomko <yury_986@mail.ru>
2023-10-20 13:43:45 +0300

    first commit

---
+++ part2/task4/src/main/java/by/tomko/ExpensesDataSource.java
@@ -0,0 +1,28 @@
+package by.tomko;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ExpensesDataSource {
+
+    private  static ExpensesDataSource dataSource;
+
+    public ExpensesDataSource () throws ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+    }
+
+    private Connection getExpensesDataSource  () throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/task4",
+                "root",
+                "root");
+    }
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        if (dataSource == null){
+            dataSource = new ExpensesDataSource();
+        }
+        return dataSource.getExpensesDataSource();
+    }
+}
+
---
+++ part2/task4/src/main/java/by/tomko/Main.java
@@ -0,0 +1,38 @@
+package by.tomko;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+
+        int idMain = Integer.parseInt(args[0]);
+        String dateMain = args[1];
+        String recipientMain = args[2];
+        int sumMain = Integer.parseInt(args[3]);
+
+        //language=MySQL
+        String insertQuery =  "insert into expenses (id,date,recipient,sum) values('"+idMain+"','"+dateMain+"','"+recipientMain+"','"+sumMain+"')";
+
+
+        Statement statement = ExpensesDataSource.getConnection().createStatement();
+        statement.executeUpdate(insertQuery);
+
+        String selectQery = "select * from expenses";
+        ResultSet resultSet = statement.executeQuery(selectQery);
+        while(resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String date = resultSet.getString("date");
+            String recipient = resultSet.getString("recipient");
+            int sum = resultSet.getInt("sum");
+            System.out.println("Id:" + id + " " + "Date:" + date + " " + "Recipient:" + recipient + " " + "Sum:" + sum);
+        }
+
+
+
+    }
+}
